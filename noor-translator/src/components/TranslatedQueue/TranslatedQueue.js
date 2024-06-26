7fe8e1aacac9d7e89151bf3a1abd3d14b3529963
Sameer Haq <sameerhaq@Sameers-MBP-3.home>
2024-06-26 13:17:45 -0500

    fix name of translated queue

--- noor-translator/src/components/TranslatedQueue/TranslatedQueue.js
+++ noor-translator/src/components/TranslatedQueue/TranslatedQueue.js
@@ -6,7 +6,7 @@ const Queue = (props) => {
     return (
         <div>
             <div className='queue-title'>
-                Queue
+                Translated Documents
             </div>
             {props.loadingCard==false ? <div className='default-queue-message'>No Files in Queue</div> : <Card name={props.file.name + "_" + props.language} size={props.file.size} setFileTwo={props.setFileOne} downloadLink={props.fileDownloadLink} downloadButton={props.downloadButtonAppear}></Card>}
            
